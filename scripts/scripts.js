a6574ccc31e89e9c256a7bb8947bcbda3b93a12b
lmalexander <elizabeth.alexander@northwestern.edu>
2019-11-23 00:31:09 -0600

    json object array and jquery table

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,26 +1,60 @@
 // Variables
-var workHours = [9, 10, 11, 12, 1, 2, 3, 4, 5];
-var rightNow = moment().format("dddd, MMMM Do");
+var todayDate = moment().format("dddd, MMMM Do");
+var workHours = [
+   {twelve: "9am" , twentyFour: "9"},
+   {twelve: "10am" , twentyFour: "10"},
+   {twelve: "11am" , twentyFour: "11"},
+   {twelve: "12pm" , twentyFour: "12"},
+   {twelve: "1pm" , twentyFour: "13"},
+   {twelve: "2pm" , twentyFour: "13"},
+   {twelve: "3pm" , twentyFour: "14"},
+   {twelve: "4pm" , twentyFour: "15"},
+   {twelve: "5pm" , twentyFour: "16"},
+];
+console.log(workHours[4].twelve);
 
 // Functions
-
-//jumbtoron current day display
+//jumbotron current day display
 function currentDay() {
-   var jumbotronDiv = $("<div>").text(rightNow);
+   var jumbotronDiv = $("<div>").text(todayDate);
    document.getElementById("currentDay");
    $("#currentDay").append(jumbotronDiv); 
 }
 currentDay();
 
 //col 1 workHours display
-function hours() { 
-    for (i = 0; i < workHours.length; i++) {
-         var hourDisplay = $("<div>").text(workHours[i]);
-         document.getElementById("hours");
-         $("#hours").append(hourDisplay); 
+
+function hours () {
+   for (i = 0; i < workHours.length; i++) {
+      var row = $("<tr/>");
+      for (i = 0; i < workHours[i].length; i++) {
+         var hourDisplay = $("<div>").text(workHours[i].twelve);
+         document.getElementById("hours").append(hourDisplay);
+      }
    }
 }
 hours();
 
 
+//function hours() { 
+    //for (i = 0; i < workHours.length; i++) {
+ //      for (i=0; i < Object.length; i++) {
+ //         var hourDisplay = $("<div>").text(workHours.twelve);
+ //         document.getElementById("hours");
+ //        $("#hours").append(hourDisplay); 
+  //     }
+
+
+   //      var hourDisplay = $("<div>").text(workHours[i]);
+         
+  // }
+//}
+//hours();
+
+// text boxes
+//function textBoxes () {
+  // var field = document.createElement("input");
+   //field.setAttribute("type", "text");
+   //document.body.appendChild("#textBox");
+// }
 
